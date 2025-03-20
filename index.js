519b0208beb71cc2710a469b043e9a984f0ba6c2
shiv <1983.shiv@gmail.com>
2025-03-20 16:59:29 +0530

    added post endpoint

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const PORT = 3000
 
 const app = express();
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
-    res.send({message: "Hello from CI CD Demo App"})
+    res.send("Hello from CI CD Demo App")
 })
+
+app.post("/api/hello", (req, res) => {
+    const name = req.query.name || "Shiv"
+    res.json({ message : `Hello ${name}`})
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`)
